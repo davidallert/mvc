277fa80f1e8f0729f215395f4f4fe33bdce44caa
David A <david.allert@hotmail.com>
2024-04-01 23:32:39 +0200

    v. 1.0.0

--- app/src/Controller/LuckyController.php
+++ app/src/Controller/LuckyController.php
@@ -26,23 +26,6 @@ class LuckyController
         );
     }
 
-    // #[Route("/api/lucky/number")]
-    // public function jsonNumber(): Response
-    // {
-    //     $number = random_int(0, 100);
-
-    //     $data = [
-    //         'lucky-number' => $number,
-    //         'lucky-message' => 'Hi there!',
-    //     ];
-
-    //     $response = new Response();
-    //     $response->setContent(json_encode($data));
-    //     $response->headers->set('Content-Type', 'application/json');
-
-    //     return $response;
-    // }
-
     #[Route("/api/lucky/number")]
     public function jsonNumber(): Response
     {
@@ -51,6 +34,32 @@ class LuckyController
         $data = [
             'lucky-number' => $number,
             'lucky-message' => 'Hi there!',
+        ];
+
+        return new JsonResponse($data);
+    }
+
+    #[Route("/api/quote")]
+    public function jsonQuote(): Response
+    {
+        $number = random_int(0, 4);
+        // $number = 0;
+        $timestamp = time();
+        $date = date('d-m-Y');
+        // En blandad kompott av citat.
+        $quotes = [
+            "Gratis är gott.",
+            "Opinion is the medium between knowledge and ignorance.",
+            "Det stod en varmkorvsgubbe ner' på Fyristorg. Han måla' korvarna svarta för han hade sorg. Han hade låda på magen, det gillas inte av lagen",
+            "He who is not contented with what he has, would not be contented with what he would like to have.",
+            "Fate leads him who follows it, and drags him who resist."
+        ];
+        $quote_of_the_day = $quotes[$number];
+
+        $data = [
+            'timestamp' => $timestamp,
+            'date' => $date,
+            'quote_of_the_day' => $quote_of_the_day
         ];
 
         return new JsonResponse($data);
